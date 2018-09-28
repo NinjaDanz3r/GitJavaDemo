3677b8e106d73155bfe440bc65557ce99097c147
Daniel <danielpejcinovski@hotmail.com>
2018-09-28 16:38:44 +0200

    Program now prints three times.

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -3,6 +3,10 @@ public class HelloWorld {
 	
 	static void printHelloWorld() {
 		System.out.println("Hello World!");
+
+		System.out.println("Hello World!");
+
+		System.out.println("Hello World!");
 	}
 	
 	public static void main(String[] args) {
